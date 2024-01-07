a69288b5ea8e9335fb5fcad44932d041c85b8884
Tomas Petrilak <tomas.petrilak@mavenir.com>
2024-01-07 22:15:08 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use libc::{
 };
 pub use libc::{AF_PACKET, IFF_PROMISC, PF_PACKET};
 
-use std::{io::Error, io::{Result, ErrorKind}, pin::Pin, task::{Poll, Context}, fmt, time::SystemTime};
+use std::{io::Error, io::{Result, ErrorKind}, pin::Pin, task::{Poll, Context}, fmt, time::SystemTime, collections::HashMap};
 use tokio::io::{unix::AsyncFd, ReadBuf, AsyncRead};
 //use futures_lite::ready;
 use std::os::unix::prelude::{AsRawFd,RawFd};
 use std::io::Read;
 use byteorder::{BigEndian, ReadBytesExt};
-
-
+use std::io::BufRead;
 
 use tokio::io::AsyncReadExt;
 
@@ -181,6 +180,13 @@ struct Stat {
     total: u64
 }
 
+impl Stat {
+    fn inc(&mut self) {
+        self.period = self.period + 1;
+        self.total = self.total + 1;
+    }
+}
+
 
 fn mac_format(i: &[u8]) -> String {
     format!("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", i[0], i[1], i[2], i[3], i[4], i[5])
@@ -210,19 +216,51 @@ fn parse(data : &Vec<u8>) -> Result<Packet>{
     })
 }
 
+fn read_leases() -> Result<std::collections::HashMap<String, String>> {
+    let file = std::fs::File::open("/tmp/dhcp.leases")?;
+    let mut map = HashMap::new();
+    for line in std::io::BufReader::new(file).lines() {
+        let lineu = line?;
+        let lineparts: Vec<&str> = lineu.split(" ").collect();
+        if lineparts.len() >= 4 {
+            map.insert(lineparts[1].to_string(), lineparts[3].to_string());
+            println!("{}", lineparts[1].to_string());
+        }
+    }
+    Ok(map)
+}
+
+fn resolve_lease(map: &std::collections::HashMap<String, String>, key: &[u8; 6]) -> String {
+    let res = map.get(&mac_format(key));
+    match res {
+        Some(v) => v.clone(),
+        None => "unknown".to_string()
+    }
+}
+
+trait MapInc {
+    fn inc(&mut self, key: [u8; 6]);
+}
+type Map = std::collections::HashMap<[u8; 6], Stat>;
+impl MapInc for Map {
+    fn inc(&mut self, key: [u8; 6]) {
+        let count = self.entry(key).or_insert(Stat{period:0,total:0});
+        count.inc();
+    }
+}
+
 async fn capture_loop(device: &str, macs: &Vec<String>, period: i32) -> Result<bool>{
     let mut afdx = AFDX::new(device).unwrap();
     let mut buf = vec![0u8; 2048];
-    let mut map = std::collections::HashMap::new();
+    let mut map = Map::new();
     let mut time_start = SystemTime::now();
     loop {
         let _r = afdx.read(buf.as_mut()).await?;
         let parsed = parse(&buf)?;
-        let count = map.entry(parsed.eth_src).or_insert(Stat{period:0,total:0});
-        count.period += 1;
-        count.total += 1;
+        map.inc(parsed.eth_src);
 
         if time_start.elapsed().unwrap().as_secs_f32() > period as f32 {
+            let leases = read_leases().unwrap();
             time_start = SystemTime::now();
             let datetime : chrono::DateTime<chrono::Local> = time_start.into();
             if macs.len() == 1 {
@@ -233,11 +271,11 @@ async fn capture_loop(device: &str, macs: &Vec<String>, period: i32) -> Result<b
                 println!("\n{}", datetime.to_rfc2822());
                 let mut table_builder = tabled::builder::Builder::default();
                 for i in &mut map {
-                    let table_column = [mac_format(i.0), i.1.total.to_string(), i.1.period.to_string()];
+                    let table_column = [mac_format(i.0), resolve_lease(&leases, i.0), i.1.total.to_string(), i.1.period.to_string()];
                     table_builder.push_record(table_column);
                     i.1.period = 0;
                 }
-                table_builder.insert_record(0, ["src", "total", "period"]);
+                table_builder.insert_record(0, ["src", "resolved", "total", "period"]);
                 let mut table = table_builder.build();
                 table.with(tabled::settings::Style::modern());
                 println!("{}", table.to_string())
